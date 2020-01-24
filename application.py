2b7cb6b934773429e08f01162a0211d79064da14
rzff <rzff.curiel@gmail.com>
2020-01-24 12:40:31 +0000

    question asnwers are now displayed as buttons

--- application.py
+++ application.py
@@ -161,15 +161,10 @@ def login():
             # Query database for teacher username
             rows = db.execute("SELECT * FROM Teacher WHERE username = :username",
                               username=request.form.get("username"))
-
-            # Set session key to teacher
-            session["key"] = "teacher"
-
         else:
             # Query database for student username
             rows = db.execute("SELECT * FROM student WHERE username = :username",
                               username=request.form.get("username"))
-            session["key"] = "student"
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -313,24 +308,8 @@ def change_password():
 
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-    # hoe komen we aan de naam van de quiz bij name= "naam"????
-    # quiz_name= session.get("quizes")
-    # catte= db.execute("SELECT category FROM new_quizes WHERE name = :name", name=quiz_name)[0]['category']
-    # diffi = db.execute("SELECT difficulty FROM new_quizes WHERE name = :name", name=quiz_name)[0]['difficulty']
-    # aantal_vragen= db.execute("SELECT amount_of_questions FROM new_quizes WHERE name = :name", name=quiz_name)[0]['amount_of_questions']
-    # Type= db.execute("SELECT type FROM new_quizes WHERE name = :name", name=quiz_name)[0]['type']
 
-    # category_dict= {"History": 23, "Politics": 24, "Geography": 22}
-    # difficulty_dict= {"easy": "easy", "medium": "medium", "hard": "hard"}
-    # cat= category_dict[catte]
-    # diff= difficulty_dict[diffi]
-    # # quiz = {"response_code":0,"results":[{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the largest city and commercial capital of Sri Lanka?","correct_answer":"Colombo","incorrect_answers":["Moratuwa","Negombo","Kandy"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which of these countries is NOT a part of the Asian continent?","correct_answer":"Suriname","incorrect_answers":["Georgia","Russia","Singapore"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which country is completely landlocked by South Africa?","correct_answer":"Lesotho","incorrect_answers":["Swaziland","Botswana","Zimbabwe"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which of these is NOT a province in China?","correct_answer":"Yangtze","incorrect_answers":["Fujian","Sichuan","Guangdong"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"The mountainous Khyber Pass connects which of the two following countries?","correct_answer":"Afghanistan and Pakistan","incorrect_answers":["India and Nepal","Pakistan and India","Tajikistan and Kyrgyzstan"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the name of one of the Neo-Aramaic languages spoken by the Jewish population from Northwestern Iraq?","correct_answer":"Lishana Deni","incorrect_answers":["Hulaul&aacute;","Lishan Didan","Chaldean Neo-Aramaic"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which of the following Inuit languages was the FIRST to use a unique writing system not based on the Latin alphabet?","correct_answer":"Inuktitut","incorrect_answers":["Inuinnaqtun","Greenlandic","Inupiat"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Where is the Luxor Hotel &amp; Casino located?","correct_answer":"Paradise, Nevada","incorrect_answers":["Las Vegas, Nevada","Winchester, Nevada","Jackpot, Nevada"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What year is on the flag of the US state Wisconsin?","correct_answer":"1848","incorrect_answers":["1634","1783","1901"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"How many countries border Kyrgyzstan?","correct_answer":"4","incorrect_answers":["3","1","6"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch is located on which Welsh island?","correct_answer":"Anglesey","incorrect_answers":["Barry","Bardsey","Caldey"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"The Hunua Ranges is located in...","correct_answer":"New Zealand","incorrect_answers":["Nepal","China","Mexico"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Fucking is a village in which country?","correct_answer":"Austria","incorrect_answers":["Germany","Switzerland","Czech Republic"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the name of the Canadian national anthem?","correct_answer":"O Canada","incorrect_answers":["O Red Maple","Leaf-Spangled Banner","March of the Puck Drop"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What North American tourist attraction is served by the &quot;Maid of the Mist&quot; tour company?","correct_answer":"Niagara Falls","incorrect_answers":["Whistler, British Columbia","Disney World","Yosemite National Park"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the name of rocky region that spans most of eastern Canada?","correct_answer":"Canadian Shield","incorrect_answers":["Rocky Mountains","Appalachian Mountains","Himalayas"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is Canada&#039;s largest island?","correct_answer":"Baffin Island","incorrect_answers":["Prince Edward Island","Vancouver Island","Newfoundland"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the name of the formerly rich fishing grounds off the island of Newfoundland, Canada?","correct_answer":"Grand Banks","incorrect_answers":["Great Barrier Reef","Mariana Trench","Hudson Bay"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"When does Finland celebrate their independence day?","correct_answer":"December 6th","incorrect_answers":["January 2nd","November 12th","February 8th"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the land connecting North America and South America?","correct_answer":"Isthmus of Panama","incorrect_answers":["Isthmus of Suez","Urals","Australasia"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which of these cities has a 4&deg; East longitude. ","correct_answer":"Amsterdam","incorrect_answers":["Rio de Janero","Toronto","Hong Kong"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"The Andaman and Nicobar Islands in South East Asia are controlled by which country?","correct_answer":"India","incorrect_answers":["Vietnam","Thailand","Indonesia"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What is the capital of Mauritius?","correct_answer":"Port Louis","incorrect_answers":["Port Moresby","Port Vila","Port-au-Prince"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"In which country is Tallinn located?","correct_answer":"Estonia","incorrect_answers":["Finland","Sweden","Poland"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Into which basin does the Jordan River flow into?","correct_answer":"Dead Sea","incorrect_answers":["Aral Sea","Caspian Sea","Salton Sea"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"The emblem on the flag of the Republic of Tajikistan features a sunrise over mountains below what symbol?","correct_answer":"Crown","incorrect_answers":["Bird","Sickle","Tree"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"The Maluku islands (informally known as the Spice Islands) belong to which country?","correct_answer":"Indonesia","incorrect_answers":["Chile","New Zealand","Fiji"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What national museum will you find in Cooperstown, New York?","correct_answer":"National Baseball Hall of Fame","incorrect_answers":["Metropolitan Museum of Art","National Toy Hall of Fame","Museum of Modern Art"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which of these cities is NOT in England?","correct_answer":"Edinburgh","incorrect_answers":["Oxford","Manchester","Southampton"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"Which country is the Taedong River in?","correct_answer":"North Korea","incorrect_answers":["South Korea","Japan","China"]}]}
 
-    #     # dit fixt wat je boven bij quiz wou
-    # main_api= "https://opentdb.com/api.php?"
-    # url= main_api + urllib.parse.urlencode({'amount': aantal_vragen}) + "&" + urllib.parse.urlencode({'category': cat}) + "&" +  urllib.parse.urlencode({'difficulty': diff}) + "&" + urllib.parse.urlencode({'type': Type})
-    # quiz = requests.get(url).json()
-    # all_questions  = (quiz['results'])
     quiz_data= db.execute("SELECT * FROM teach_lijst")[0]
     quiz_answers = quiz_data['all_answer_sheets']
     quiz_questions = quiz_data["vragen_lijst"]
@@ -393,11 +372,9 @@ def quiz():
         question = question_list[new_question]
         answer_list =  all_answer_sheets[new_question]
         answer_list = random.sample(answer_list, len(answer_list))
-
+        print(answer)
         if answer in correct_answers:
             session["result"] += 1
-            print(session["result"])
-
         if new_question != 0:
             return render_template("quiz.html", question = question , answers = answer_list)
         else:
