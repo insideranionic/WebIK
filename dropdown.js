a3c25c42c673e946667cd28775efcd8b3c028f2e
merijn212 <you@example.com>
2020-01-27 09:19:00 +0000

    navbar changes

--- dropdown.js
+++ dropdown.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-    $(".teacher").click(function () {
-       //this is change select value 1
-        $('#dynamicChange').val('1').trigger('change');
-    });
-     $(".student").click(function () {
-         //
-         //this is change select value 1
-         $('#dynamicChange').val('2').trigger('change');
-    });
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
 
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+
+
+window.addEventListener('load', function() {
+  var role = window.location.search.split('=')[1].split(',');
+  if (role) {
+    document.getElementById('selectRole').value = role;
+  }
 });
